58a8ae9dea3d196d7e115c1e31da1c5ff73a2720
bmkim621 <moonstruck0614@gmail.com>
2018-11-27 16:33:10 +0900

    MyBatis 기본작업 완료

---
+++ src/main/java/kr/or/yi/mybatis_c3p0_kbm/dto/Title.java
@@ -0,0 +1,26 @@
+package kr.or.yi.mybatis_c3p0_kbm.dto;
+
+public class Title {
+	private int code;
+	private String name;
+	
+	//getters, setters
+	public int getCode() {
+		return code;
+	}
+	public void setCode(int code) {
+		this.code = code;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+	//toString
+	@Override
+	public String toString() {
+		return String.format("Title [code=%s, name=%s]", code, name);
+	}
+}
